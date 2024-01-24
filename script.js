6bb4fc13e093de2ae65900759aba31ab40bcf734
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-01-25 00:09:07 +0530

    Updated functionality to remove and filter items

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemform = document.querySelector('#item-form');
 const iteminput = document.querySelector('#item-input');
 const itemlist = document.querySelector('#item-list');
+const clearbtn = document.querySelector('#clear');
+const filter = document.querySelector('#filter');
 
 function createButton(classes){
     const btn = document.createElement('button');
@@ -32,8 +34,56 @@ function addItem(e){
 
     // add to list
     itemlist.appendChild(li);
+    checkUI();
     iteminput.value = '';
+}
+
+function removeItem(e){
+    if(e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove();
+        checkUI();
+    }
+}
+
+function clearItems(e){
+    if(confirm('Are you sure?')){
+        while(itemlist.firstChild){
+            itemlist.removeChild(itemlist.firstChild);
+        }
+    }
+    checkUI();
+}
+
+
+function checkUI(){
+     const items = itemlist.querySelectorAll('li');
+     if(items.length === 0){
+        filter.style.display = 'none';
+        clearbtn.style.display = 'none';
+     } else {
+        filter.style.display = 'block';
+        clearbtn.style.display = 'block';
+     }
+}
+
+function filteritems(e){
+    let value = filter.value.toLowerCase();
+    itemlist.querySelectorAll('li').forEach(n => {
+        if(n.innerText.toLowerCase().indexOf(value) === -1){
+            n.style.display = 'none';
+        }else{
+            n.style.display = 'flex';
+        }
+    })
 }
 
 // Event Listeners
 itemform.addEventListener('submit', addItem);
+
+itemlist.addEventListener('click', removeItem);
+
+clearbtn.addEventListener('click', clearItems);
+
+filter.addEventListener('input', filteritems)
+
+checkUI();
